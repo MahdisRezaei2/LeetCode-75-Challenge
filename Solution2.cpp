47b021212e66d3db8593620f21e9fc469f97e713
MahdisRezaei2 <mahdis.re20@gmail.com>
2023-10-31 13:41:59 -0700

    Commit new solutions for the first and second LeetCode 75 ds Challenge

---
+++ Solution2.cpp
@@ -0,0 +1,29 @@
+using namespace std;
+#include <iostream>
+#include <vector>
+
+class Solution2
+{
+public:
+    string mergeAlternately(string word1, string word2)
+    {
+        string merged;
+        int i = 0, j = 0;
+
+        while (i < word1.size() || j < word2.size())
+        {
+            if (i < word1.size())
+            {
+                merged += word1[i];
+                i++;
+            }
+            if (j < word2.size())
+            {
+                merged += word2[j];
+                j++;
+            }
+        }
+
+        return merged;
+    }
+};
